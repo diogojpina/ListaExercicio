befeabe96b9a7ed79ad682a8c1fba98937084c8d
rafael <rafael@rafael-Inspiron-N4050>
2012-03-28 17:40:42 -0300

    adicionado testes no metodo autenticação de LoginController

---
+++ Testes/br/usp/ime/academicdevoir/controller/LoginControllerTeste.java
@@ -0,0 +1,63 @@
+package br.usp.ime.academicdevoir.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import br.com.caelum.vraptor.util.test.MockResult;
+import br.usp.ime.academicdevoir.dao.UsuarioDao;
+import br.usp.ime.academicdevoir.entidade.Usuario;
+import br.usp.ime.academicdevoir.infra.Privilegio;
+import br.usp.ime.academicdevoir.infra.UsuarioSession;
+
+public class LoginControllerTeste {
+	
+	private LoginController loginController;
+	private Usuario usuario;
+	
+	
+	private MockResult result;
+
+	private UsuarioDao usuarioDao;
+	private UsuarioSession usuarioSession;
+	
+	
+	@Before
+	public void setUp(){
+		usuarioDao = Mockito.mock(UsuarioDao.class);
+		result = Mockito.spy(new MockResult());
+		
+		usuarioSession = new UsuarioSession(); 
+		usuario = new Usuario();
+		usuario.setLogin("alunor");
+		usuario.setEmail("usuarior@ime.usp.br");
+		usuario.setPrivilegio(Privilegio.ALUNO);
+		usuario.setSenha("alunor");
+		usuarioSession.setUsuario(usuario);
+		loginController = new LoginController(result, usuarioDao, usuarioSession);
+		
+	
+	}
+	
+	@Test
+	public void deveFazerLogin(){
+		loginController.login();
+	}
+	
+	@Test
+	public void deveAutenticarLogin(){
+		Mockito.when(usuarioDao.fazLogin(usuario.getLogin(), usuario.getSenha())).thenReturn(usuario);
+		loginController.login(usuario);
+	}
+	
+	@Test
+	public void naoDeveAutenticar() {
+		Mockito.when(usuarioDao.fazLogin(usuario.getLogin(), usuario.getSenha())).thenReturn(null);
+		loginController.login(usuario);	
+		Mockito.verify(result).include("error", "Login ou senha incorreta!");
+		
+	}
+
+	
+	
+}
