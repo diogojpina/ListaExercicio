56457e03c7401d37b532bc5b3db6c6ec3c566ad4
ravila <ravila.santos@gmail.com>
2012-04-01 00:52:23 -0300

    Tentando arrumar o TurmasDaoTest. Ainda sem sucesso.

--- Testes/br/usp/ime/academicdevoir/dao/TurmasDaoTest.java
+++ Testes/br/usp/ime/academicdevoir/dao/TurmasDaoTest.java
@@ -3,10 +3,11 @@ package br.usp.ime.academicdevoir.dao;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.hibernate.Session;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.Transaction;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,46 +16,80 @@ import static org.mockito.Mockito.*;
 import org.mockito.MockitoAnnotations;
 
 import br.usp.ime.academicdevoir.entidade.Aluno;
+import br.usp.ime.academicdevoir.entidade.Disciplina;
 import br.usp.ime.academicdevoir.entidade.Turma;
 
 public class TurmasDaoTest {
 
-	Turma turmaNaoCadastrada = new Turma();
-	Turma turmaMAC110 = new Turma();
-	Turma turmaMAC122 = new Turma();
-	Aluno aluno = new Aluno();
-	List<Turma> turmasList = new ArrayList<Turma>();
+	List<Turma> turmasList;
+	Turma turmaMAC110;
+	Turma turmaMAC122;
+	Turma turmaNaoCadastrada;
+	Aluno aluno;
+	Disciplina disciplina;
 	TurmaDao turmaDao;
-	AlunoDao alunoDao;
 	private @Mock Session session;
+	private @Mock Transaction tx;
 	
 	@Before
-	public void preparaTestes(){
-		turmaMAC110.setId(110L);
-		turmaMAC122.setId(122L);
-		turmaNaoCadastrada.setId(1L);
-		aluno.setId(666L);
-		session = new Configuration().configure().buildSessionFactory().openSession();
+	public void setUp(){
+		MockitoAnnotations.initMocks(this);
+		when(session.beginTransaction()).thenReturn(tx);
 		turmaDao = new TurmaDao(session);
-		alunoDao = new AlunoDao(session);
+		
+		turmasList = new ArrayList<Turma>();
+		turmaMAC110 = new Turma();
+		turmaMAC122 = new Turma();
+		turmaNaoCadastrada = new Turma();
+		disciplina = new Disciplina();
+		aluno = new Aluno();
+		aluno.setId(666L);
+	}
+	
+	public void shouldSaveTurma(){
+		criaTurmas();
+		turmaDao.salvaTurma(turmaMAC110);
+		
+		/*turmaDao.salvaTurma(turmaMAC122);
+		turmaDao.salvaTurma(turmaNaoCadastrada);
+		*/
+		verify(session).save(turmaMAC110);
 	}
 	@Test
 	public void listaFiltrada(){
 		List<Turma> naoCadastradas = new ArrayList<Turma>();
 
-		turmaDao.salvaTurma(turmaMAC110);
-		turmaDao.salvaTurma(turmaMAC122);
-		turmaDao.salvaTurma(turmaNaoCadastrada);
-		turmasList.add(turmaMAC110);
-		turmasList.add(turmaMAC122);
-		aluno.setTurmas(turmasList);
-		alunoDao.salvaAluno(aluno);
+		criaTurmas();
+		
 		
 		Long alunoId = aluno.getId();
-		naoCadastradas = turmaDao.listaTurmasFiltradas(alunoId);
+		naoCadastradas = mock(TurmaDao.class).listaTurmasFiltradas(alunoId);//turmaDao.listaTurmasFiltradas(alunoId);
 		
 		assertEquals(1, naoCadastradas.size());
 		
 		assertEquals(turmaNaoCadastrada, naoCadastradas.get(0));
+	}
+	
+	private void criaTurmas() {
+		Collection<Aluno> alunos = new ArrayList<Aluno>();
+		alunos.add(aluno);
+		disciplina.setId(2L);
+		
+		turmaMAC110.setId(110L);
+		turmaMAC110.setAlunos(alunos);
+		turmaMAC110.setDisciplina(disciplina);
+		turmaMAC110.setNome("MAC110");
+		turmaMAC122.setId(122L);
+		turmaMAC122.setAlunos(alunos);
+		turmaMAC122.setDisciplina(disciplina);
+		turmaMAC122.setNome("MAC122");
+		turmaNaoCadastrada.setId(1L);
+		turmaNaoCadastrada.setDisciplina(disciplina);
+		turmaNaoCadastrada.setNome("naoCadastrada");
+		
+		turmaDao.salvaTurma(turmaMAC110);
+		
+		turmaDao.salvaTurma(turmaMAC122);
+		turmaDao.salvaTurma(turmaNaoCadastrada);
 	}
 }
