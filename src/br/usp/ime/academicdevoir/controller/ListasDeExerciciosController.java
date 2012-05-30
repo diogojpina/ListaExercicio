e3db1a22f315a999c3e0a6fbd0784d34c13d7397
Laécio Freitas Chaves <laeciofreitas@gmail.com>
2012-05-30 16:49:06 -0300

    (Renato/Laécio) adicionado filtro de disciplinas na listagem de questões

--- src/br/usp/ime/academicdevoir/controller/ListasDeExerciciosController.java
+++ src/br/usp/ime/academicdevoir/controller/ListasDeExerciciosController.java
@@ -626,8 +626,8 @@ public class ListasDeExerciciosController {
 		primeiroReg = (proxPagina - 1) * Constantes.NUM_REGISTROS_PAGINA;
 
 		listaDeQuestoesPaginadas = questaoDao.listaPaginada(primeiroReg,
-				Constantes.NUM_REGISTROS_PAGINA, filtro);
-		ultimaPagina = questaoDao.tamanhoDaLista(filtro)
+				Constantes.NUM_REGISTROS_PAGINA, filtro, id);
+		ultimaPagina = questaoDao.tamanhoDaLista(filtro, id)
 				/ Constantes.NUM_REGISTROS_PAGINA;
 		if (listaDeQuestoesPaginadas.size() % Constantes.NUM_REGISTROS_PAGINA != 0)
 			ultimaPagina++;
